17a75b30c626575baf6386af4027a625d295d33c
FEB <beccali.fabio4@gmail.com>
2025-12-09 11:50:05 +0100

    moved header section of backend scripts to a separete file (cors.php)

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,9 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("content-type: application/json; charset=UTF-8");
+require_once("cors.php");
 
 
 //retrieves the JSON data from the request body
--- backend/api/register.php
+++ backend/api/register.php
@@ -1,10 +1,6 @@
 <?php
 //header shii
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("content-type: application/json; charset=UTF-8");
+require_once("cors.php");
 
 $json_data = file_get_contents('php://input');
 
