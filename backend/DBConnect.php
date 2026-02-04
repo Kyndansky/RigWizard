784e30a9b2037ebe1eccb80b77248abea01fa162
Davide Riccobene <davidericcobene10@gmail.com>
2026-02-04 19:04:16 +0100

    fixed api files in api/games/ directory not working (see details in extended description)
    basically specified the charset the database connection should use: before italic was used as default and that corrupted the result if the handled data contained a character not supported from encoding outside of UTF-8. this problem was hard to find because both in my own windows and linux installations it didn't occur: i had to use the xampp version used in school to replicate the problem

--- backend/DBConnect.php
+++ backend/DBConnect.php
@@ -4,6 +4,7 @@ $dbUsername = "root";
 $password = "";
 $dbName = "rigwizard";
 $dbConnection = new mysqli($hostname, $dbUsername, $password, $dbName);
+$dbConnection->set_charset("utf8mb4");
 if ($dbConnection->connect_error) {
     die("Connection failed: " . $dbConnection->connect_error);
 }
